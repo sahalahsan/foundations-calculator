f93e340e5549c708142c848d9053b68e5e84a2fb
sahalahsan <sahaledu000@gmail.com>
2023-02-18 20:03:03 -0500

    Complete code files which works as the instructions suggest.

--- javascript.js
+++ javascript.js
@@ -1,48 +1,77 @@
 // Accessing various elements
 var digits = document.querySelectorAll(".digit");
 var display = document.querySelector(".display");
-var clear = document.querySelector(".clear");
+var equal = document.querySelector(".equal")
 var operators = document.querySelectorAll(".operator");
-var equal = document.querySelector(".equal");
 
 // Variables 
 let a;
 let b;
 let operator;
+let operatorVariable = 0;
+let lastOperator;
 
 // Making operators appear on display
 Array.from(operators).forEach( item => {
-    item.addEventListener("click", (e) => {
-      operator = e.target.textContent;
-      console.log(operator);
-      a = display.textContent;
-      console.log(a);
-      display.textContent +=  e.target.textContent; 
-    });
+  item.addEventListener("click", (e) => {
+    operator = e.target.textContent;
+    operatorVariable++;
+    if(display.textContent == ""){
+      a = +(display.textContent);
+      lastOperator = operator //operator
+      display.textContent = "0" + operator;
+    } else if(operatorVariable > 1){
+      var allDigitsBeforeOperator  =(display.textContent).slice(0,display.textContent.length-1);
+      display.textContent = allDigitsBeforeOperator;
+      lastOperator = operator //operator
 
+      a = +(display.textContent);
+      display.textContent += e.target.textContent; 
+    } else if(operatorVariable === 1 && 
+      (display.textContent.includes(`${lastOperator}`) == true)){
+      a = +(display.textContent.split(`${lastOperator}`)[0]);
+      operator = lastOperator;
+      b = +(display.textContent.split(`${lastOperator}`)[1]);
+      operate();
+      // display.textContent += e.target.textContent;
+      operatorVariable = 0;
+    
+    } else {
+      a = +(display.textContent);
+      lastOperator = operator  
+      display.textContent +=  e.target.textContent; 
+    }
+  });
 });
 
 // making digits appear on display
 Array.from(digits).forEach( item => {
-    item.addEventListener("click", (e) => {
-      display.textContent += e.target.textContent;
-    });
-    
+  item.addEventListener("click", (e) => {
+    display.textContent += e.target.textContent;
+    operatorVariable = 0;
+  });
 });
 
-//For numberB
+// Make point appear on display(second appearance disabled)
+document.querySelector(".decimal").addEventListener("click", (e) => {
+  if(display.textContent == ""){
+    display.textContent = "0" + e.target.textContent;
+  } else if(display.textContent.includes(".")){
+    display.textContent = display.textContent;
+  } else {
+    display.textContent += e.target.textContent;
+  }
+});
+
+//Accessing number placed after operator
 equal.addEventListener("click", (e)=> {
-    let dis = display.textContent;
-    let splitArr = dis.split(`${operator}`);
-    b = splitArr[1];
-    console.log(b);
+  let dis = display.textContent;
+  let splitArr = dis.split(`${operator}`);
+  b = +(splitArr[1]);
 }); 
 
-
-
 // Removes Content from display
-clear.addEventListener("click", () => display.textContent = "");
-
+document.querySelector(".clear").addEventListener("click", () => display.textContent = "");
 
 // Addition function
 function add(a, b){
@@ -61,23 +90,37 @@ function multiply(a, b){
 
 // Division function 
 function div(a, b){
+  if(b == 0){
+    alert("You can't divide by 0!");
+  } else {
   return a / b;
+  }
 };
 
-// Operate function
+//Calling Operate function
 equal.addEventListener("click", (e) => {
-  console.log(a);
-  console.log(b);
-  console.log(operator);
+  if(display.textContent !== "") {
+    if(a !== undefined && b !== NaN){
+      operate();
+    }
+  }
+});
+
+// Operate function
+function operate(){
   if(operator == "+"){
     var result = add(a, b);
   }else if(operator == "-"){
     var result = sub(a, b);
   } else if(operator == "÷"){
     var result = div(a, b);
-    console.log("oja");
   } else {
     var result = multiply(a, b);
   }
-  display.textContent = result;
-});
+
+  if(!Number.isInteger(result) && result.toString().split('.')[1].length > 3){
+    display.textContent = result.toFixed(3);
+  } else {
+    display.textContent = result;
+  }
+};
